92ec06964638c1fec06267cc4a7813ab4b42d9bd
Clarita Flor <clara.flores.s@gmail.com>
2019-08-17 17:05:42 -0400

    modifico

--- holaMundo.ts
+++ holaMundo.ts
@@ -6,4 +6,5 @@ console.log ("Hola github");
 console.log ("Feliz dia de la mujer :3");
 console.log ("Hola github");
 console.log ("Hola Bienvenidos al taller de Git y GitHub")
+console.log ("Hola Bienvenidos al taller de Git y GitHub*************")
 
